cc8d5ba1e31340c6b22a88b93a5c0a6ef85d1855
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-09 13:02:22 +0300

    Added json attributes to user model

--- NonRelationalDatabaseGoal/Models/User.cs
+++ NonRelationalDatabaseGoal/Models/User.cs
@@ -1,12 +1,24 @@
-﻿namespace NonRelationalDatabaseGoal.Models;
+﻿using Newtonsoft.Json;
+
+namespace NonRelationalDatabaseGoal.Models;
 
 public class User
 {
-    public string Id { get; set; } = default!;
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; } = string.Empty;
 
+    [JsonProperty(PropertyName = "userName")]
     public string UserName { get; set; } = default!;
+
+    [JsonProperty(PropertyName = "firstName")]
     public string FirstName { get; set; } = default!;
+
+    [JsonProperty(PropertyName = "lastName")]
     public string LastName { get; set; } = default!;
+
+    [JsonProperty(PropertyName = "profession")]
     public string? Profession { get; set; }
+
+    [JsonProperty(PropertyName = "specialization")]
     public string? Specialization { get; set; }
 }
