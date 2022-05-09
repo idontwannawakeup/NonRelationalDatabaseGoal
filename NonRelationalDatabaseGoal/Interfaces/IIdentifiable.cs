88a6a8c037a1ae0ca2f04f9854a22ca1ce1eed80
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-09 18:58:14 +0300

    Implemented generic service for CRUD operations

---
+++ NonRelationalDatabaseGoal/Interfaces/IIdentifiable.cs
@@ -0,0 +1,6 @@
+﻿namespace NonRelationalDatabaseGoal.Interfaces;
+
+public interface IIdentifiable
+{
+    string Id { get; set; }
+}
---
+++ NonRelationalDatabaseGoal/Services/GenericService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Cosmos;
+using NonRelationalDatabaseGoal.Extensions;
+using NonRelationalDatabaseGoal.Interfaces;
+
+namespace NonRelationalDatabaseGoal.Services;
+
+public class GenericService<T> where T : IIdentifiable
+{
+    protected Container Container { get; }
+
+    public GenericService(Container container) => Container = container;
+
+    public async Task<IEnumerable<T>> GetAllAsync() => await Container.GetAllAsync<T>();
+
+    public async Task<T> GetByIdAsync(string id) => await Container.ReadItemAsync<T>(
+        id, new PartitionKey(id));
+
+    public async Task CreateAsync(T item) => await Container.CreateItemAsync(
+        item, new PartitionKey(item.Id));
+
+    public async Task UpdateAsync(T item) => await Container.UpsertItemAsync(
+        item, new PartitionKey(item.Id));
+
+    public async Task DeleteAsync(string id) => await Container.DeleteItemAsync<T>(
+        id, new PartitionKey(id));
+}
