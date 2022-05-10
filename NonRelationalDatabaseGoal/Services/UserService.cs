bd341402738adf0f940f9b4036440addd1306f45
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-10 16:41:16 +0300

    Inherited user service from generic service

--- NonRelationalDatabaseGoal/Services/UserService.cs
+++ NonRelationalDatabaseGoal/Services/UserService.cs
@@ -3,23 +3,9 @@ using NonRelationalDatabaseGoal.Extensions;
 
 namespace NonRelationalDatabaseGoal.Services;
 
-public class UserService
+public class UserService : GenericService<Models.User>
 {
-    private readonly Container _usersContainer;
-
-    public UserService(CosmosClient client) => _usersContainer = client.GetUsersContainer();
-
-    public async Task<IEnumerable<Models.User>> GetAllAsync() => await _usersContainer.GetAllAsync<Models.User>();
-
-    public async Task<Models.User> GetByIdAsync(string id) => await _usersContainer.ReadItemAsync<Models.User>(
-        id, new PartitionKey(id));
-
-    public async Task CreateAsync(Models.User user) => await _usersContainer.CreateItemAsync(
-        user, new PartitionKey(user.Id));
-
-    public async Task UpdateAsync(Models.User user) => await _usersContainer.UpsertItemAsync(
-        user, new PartitionKey(user.Id));
-
-    public async Task DeleteAsync(string id) => await _usersContainer.DeleteItemAsync<Models.User>(
-        id, new PartitionKey(id));
+    public UserService(CosmosClient client) : base(client.GetUsersContainer())
+    {
+    }
 }
