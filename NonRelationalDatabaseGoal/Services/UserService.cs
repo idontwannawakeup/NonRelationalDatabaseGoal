ecb558bd114d2da368c681bf6b022d745cdda4a1
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-12 16:00:30 +0300

    Implemented ordering and pagination for users

--- NonRelationalDatabaseGoal/Services/UserService.cs
+++ NonRelationalDatabaseGoal/Services/UserService.cs
@@ -1,5 +1,7 @@
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
 using NonRelationalDatabaseGoal.Extensions;
+using NonRelationalDatabaseGoal.Parameters;
 
 namespace NonRelationalDatabaseGoal.Services;
 
@@ -8,4 +10,11 @@ public class UserService : GenericService<Models.User>
     public UserService(CosmosClient client) : base(client.GetUsersContainer())
     {
     }
+
+    public async Task<IEnumerable<Models.User>> GetAsync(QueryStringParameters parameters) =>
+        await Container.GetItemLinqQueryable<Models.User>()
+            .ApplyOrdering(parameters)
+            .ApplyPagination(parameters)
+            .ToFeedIterator()
+            .ReadAllAsync();
 }
