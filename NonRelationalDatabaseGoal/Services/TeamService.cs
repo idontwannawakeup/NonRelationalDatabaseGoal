84be97209ffd0857060231a3357591323286e9ac
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-12 13:43:31 +0300

    Implemented new methods in TeamService

--- NonRelationalDatabaseGoal/Services/TeamService.cs
+++ NonRelationalDatabaseGoal/Services/TeamService.cs
@@ -15,11 +15,39 @@ public class TeamService : GenericService<Team>
 
     public override async Task CreateAsync(Team team)
     {
-        var leaderResponse = await UsersContainer.ReadItemAsync<Models.User>(team.LeaderId, new PartitionKey(team.LeaderId));
-        var leader = leaderResponse.Resource;
+        Models.User leader = await UsersContainer.ReadItemAsync<Models.User>(
+            team.LeaderId,
+            new PartitionKey(team.LeaderId));
+
         leader.Teams.Add(team.Id);
         team.Members.Add(leader.Id);
         await base.CreateAsync(team);
         await UsersContainer.UpsertItemAsync(leader, new PartitionKey(leader.Id));
+    }
+
+    public async Task AddMemberAsync(string teamId, string memberId)
+    {
+        Team team = await base.GetByIdAsync(teamId);
+        Models.User member = await UsersContainer.ReadItemAsync<Models.User>(
+            memberId,
+            new PartitionKey(memberId));
+
+        team.Members.Add(memberId);
+        member.Teams.Add(teamId);
+        await base.UpdateAsync(team);
+        await UsersContainer.UpsertItemAsync(member, new PartitionKey(member.Id));
+    }
+
+    public async Task DeleteMemberAsync(string teamId, string memberId)
+    {
+        Team team = await base.GetByIdAsync(teamId);
+        Models.User member = await UsersContainer.ReadItemAsync<Models.User>(
+            memberId,
+            new PartitionKey(memberId));
+
+        team.Members.Remove(memberId);
+        member.Teams.Remove(teamId);
+        await base.UpdateAsync(team);
+        await UsersContainer.UpsertItemAsync(member, new PartitionKey(member.Id));
     }
 }
